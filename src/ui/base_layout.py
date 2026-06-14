ae20a60e7e7a7c93d1718ee70fc9b13cc664ae88
ABHISHEK SINGH DASILA <abhisheksinghdasila7@gmail.com>
2026-06-14 13:11:38 +0530

    home

---
+++ src/ui/base_layout.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+def style_base_layout():
+
+    st.markdown(
+        """
+    <style>
+        .stApp
+        {
+        background: #5865F2 !important;
+        }
+    </style>
+""", unsafe_allow_html=True
+    )
